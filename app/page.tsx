876f7c31d7d866976439829529db2682dc458ce4
anuj <anujsbhuyar@gmail.com>
2024-09-23 17:37:56 +0530

    added testimonial

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Features from "@/components/Features";
 import { BackgroundCellAnimation } from "@/components/Hero";
 import { FloatingNavDemo } from "@/components/Navbar";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <FloatingNavDemo />
       <BackgroundCellAnimation />
       <Features />
+      <Testimonials />
     </div>
   );
 }
