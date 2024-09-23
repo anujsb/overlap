5d763fc2f2ad9b3e987c8c757da7450ec99c8cc5
anuj <anujsbhuyar@gmail.com>
2024-09-23 17:36:31 +0530

    added features page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Features from "@/components/Features";
 import { BackgroundCellAnimation } from "@/components/Hero";
 import { FloatingNavDemo } from "@/components/Navbar";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto">
       <FloatingNavDemo />
       <BackgroundCellAnimation />
+      <Features />
     </div>
   );
 }
